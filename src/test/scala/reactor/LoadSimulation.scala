304fe13404979ea4bd64ac766e26dd352b1bb0dc
张羽 <zhangyu21@staff.weibo.com>
2019-05-27 16:44:23 +0800

    init

--- src/test/scala/reactor/LoadSimulation.scala
+++ src/test/scala/reactor/LoadSimulation.scala
@@ -8,34 +8,42 @@ import scala.concurrent.duration._
 class LoadSimulation extends Simulation {
 
   // 从系统变量读取 baseUrl、path和模拟的用户数
-  //  val baseUrl = System.getProperty("base.url")
-  //  val testPath = System.getProperty("test.path")
-  //  val simUsers = System.getProperty("sim.users").toInt
-  val baseUrl = "http://10.77.9.40:10000"
-  val path = System.getProperty("path")
   val latency = 100
-  val testPath = "/" + path + "?latency=" + latency
-  val simUsers = 5000
-  val requestName = simUsers + "_" + path + "_" + latency
+  //  val baseUrl = System.getProperty("base.url")
+  val baseUrl = "http://10.77.9.40:10000"
+  val uri = System.getProperty("uri")
+  //  val path = System.getProperty("path")
+  val path = "?latency=" + latency
+  val qps = System.getProperty("qps").toInt
+  val duration = System.getProperty("duration").toInt
+  val pause = duration + System.getProperty("pause").toInt
 
-  val httpConf = http.baseUrl(baseUrl)
+  //  val uriPath = uri + path
+  //  val requestName = qps + "_" + uri
 
-  // 定义模拟的请求
-  val helloRequest = during(30) {
-    // 自定义测试名称
-    exec(http(requestName)
-      // 执行get请求
-      .get(testPath))
-      // 模拟用户思考时间，随机1~2秒钟
-      .pause(1 second)
-  }
+  val httpConf = http.warmUp(baseUrl).baseUrl(baseUrl)
 
   // 定义模拟的场景
-  val scn1 = scenario("hello")
-    // 该场景执行上边定义的请求
-    .exec(helloRequest)
+  val uri1 = "/hello01"
+  val scn1 = scenario(uri1).exec(http(qps + "_" + uri1).get(uri1 + path))
+  val uri2 = "/hello02"
+  val scn2 = scenario(uri2).exec(http(qps + "_" + uri2).get(uri2 + path))
+  val uri3 = "/hello03"
+  val scn3 = scenario(uri3).exec(http(qps + "_" + uri3).get(uri3 + path))
+  val uri4 = "/hello04"
+  val scn4 = scenario(uri4).exec(http(qps + "_" + uri4).get(uri4 + path))
+  val uri5 = "/hello05"
+  val scn5 = scenario(uri5).exec(http(qps + "_" + uri5).get(uri5 + path))
+  val uri6 = "/hello06"
+  val scn6 = scenario(uri6).exec(http(qps + "_" + uri6).get(uri6 + path))
 
+  setUp(scn1.inject(constantUsersPerSec(qps) during (duration seconds)),
+    scn2.inject(nothingFor(pause seconds), constantUsersPerSec(qps) during (duration seconds)),
+    scn3.inject(nothingFor(pause * 2 seconds), constantUsersPerSec(qps) during (duration seconds)),
+    scn4.inject(nothingFor(pause * 3 seconds), constantUsersPerSec(qps) during (duration seconds)),
+    scn5.inject(nothingFor(pause * 4 seconds), constantUsersPerSec(qps) during (duration seconds)),
+    scn6.inject(nothingFor(pause * 5 seconds), constantUsersPerSec(qps) during (duration seconds))
+  ).protocols(httpConf)
   // 配置并发用户的数量在30秒内均匀提高至sim_users指定的数量
-  setUp(scn1.inject(rampUsers(simUsers).during(10 seconds))
-    .protocols(httpConf))
+  //  setUp(scn1.inject(rampUsers(simUsers).during(10 seconds)).protocols(httpConf))
 }
